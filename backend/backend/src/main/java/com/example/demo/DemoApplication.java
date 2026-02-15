dd5ee72b06bae5eca1500eabbbb6d38cf48caaee
navodya-03 <nawodhyasrilanka2015@gmail.com>
2026-02-16 01:01:59 +0530

    Refactor: moving files to com.example.demo package from com.agrolink

--- backend/backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/backend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class DemoApplication {
